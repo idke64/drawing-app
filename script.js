0dc594b9ca1019d0004d34ddfbceaf155e98f1bd
Leaves333 <canchenli@gmail.com>
2023-06-03 11:09:31 -0500

    display camera and canvas (but i broke drawing)

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var drawing = false;
 var context = canvas.getContext("2d");
 
 function onLoad(){
-    canvas.width = window.innerWidth * .75
-    canvas.height = window.innerHeight * .75
+    canvas.width = window.innerWidth
+    canvas.height = canvas.width * 9 / 16;
     context.fillStyle="#fff";
     context.fillRect(0,0,canvas.width,canvas.height);
 }
@@ -16,7 +16,7 @@ window.addEventListener("load",onLoad());
 function draw(e){
     if (drawing){
         context.lineTo(e.offsetX, e.offsetY);
-        console.log(e.offsetX);
+        //console.log(e.offsetX);
         context.stroke();
     }
 }
@@ -35,3 +35,16 @@ function stopDrawing(){
 canvas.addEventListener("mousedown",startDrawing);
 canvas.addEventListener("mousemove",draw);
 canvas.addEventListener("mouseup",stopDrawing);
+
+// video things
+var video = document.querySelector("#videoElement")
+
+if (navigator.mediaDevices.getUserMedia) {
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+    video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+    console.log("Something went wrong!");
+    });
+}
